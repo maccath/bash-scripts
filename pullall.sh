f9e5581aaa7387d833a40b7396bf7996b6a2dc45
Katy Ereira <katy.ereira@castletrust.co.uk>
2016-02-24 10:50:52 +0000

    Add script to pull specified branch in all repositories.

---
+++ pullall.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Example usage:
+# ./pullall.sh [branchName] [branchName] ...
+
+# Configuration file
+source ./configuration.cfg
+
+# Move to directory containing all projects
+cd $project_path;
+
+# For each directory (project)
+for d in */ ; do
+    cd "$d";
+
+    # Pull current branch
+    git pull;
+    original_branch= git rev-parse --abbrev-ref HEAD;
+
+    # For each specified branch, if it exists, pull
+    for b in "$@"; do
+        if [ "git branch --list ${b}" ]; then
+           git checkout $b;
+           git pull;
+        fi
+    done
+
+    # Go back to original branch
+    git checkout $original_branch;
+
+    cd ..;
+done
