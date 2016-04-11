838489530ea97dabdef29601edc66fc60bc3cc31
Katy Ereira <katy.ereira@castletrust.co.uk>
2016-04-11 17:12:18 +0100

    Trim trailing slashes to prevent unintended behaviour if using tab completion from within the project directory.

--- bin/ct.sh
+++ bin/ct.sh
@@ -9,7 +9,10 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 # Configuration file
 source ${parent_path}/../etc/configuration.cfg
 
-project=$(ls -d ${project_path}/${1}* | head -n1)
+# Trim trailing slashes
+trimmed=$(echo ${1} | sed 's:/*$::')
+
+project=$(ls -d ${project_path}/${trimmed}* | head -n1)
 
 if [ "-d ${project}" ]; then
     cd ${project}
